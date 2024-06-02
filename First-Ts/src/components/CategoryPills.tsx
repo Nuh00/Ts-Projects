30e47740af18cd82f9aef0321f8b72ebc2c126af
Nuh00 <barkatnoah35@gmail.com>
2024-06-02 13:00:33 -0500

    update

--- First-Ts/src/components/CategoryPills.tsx
+++ First-Ts/src/components/CategoryPills.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import Button from "./Button";
+import { ChevronLeft } from "lucide-react";
 
-function CategoryPills() {
+type CategoryPillsProps = {
+  categories: string[];
+};
+
+function CategoryPills({ categories }: CategoryPillsProps) {
+  const [activeCategory, setActiveCategory] = useState(categories[0]);
   return (
     <div className="overflow-x-hidden relative">
-      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
-        <Button className="px-3 py-1 rounded-lg whitespace-nowrap" variant="dark">All</Button>
+      <div className="flex whitespace-nowrap gap-3 transition-transform  w-[max-content]">
+        {categories.map((category) => (
+          <Button
+            onClick={() => setActiveCategory(category)}
+            key={category}
+            variant={category === activeCategory ? "dark" : "default"}
+            className="py-1 px-3 rounded-lg whitespace-nowrap"
+          >
+            {category}
+          </Button>
+        ))}
+      </div>
+      <div>
+        <Button size="icon" variant="ghost" className="">
+          <ChevronLeft />
+        </Button>
       </div>
     </div>
   );
--- First-Ts/src/layouts/Hero.tsx
+++ First-Ts/src/layouts/Hero.tsx
@@ -1,11 +1,19 @@
 import CategoryPills from "../components/CategoryPills";
+import { categories } from "../data/home";
+
+type CategoryArrProps = {
+  categories: string[];
+};
 
 function Hero() {
   return (
     <div className="grid grid-cols-[auto,1fr] flex-grow overflow-auto">
       <aside>Sidebar</aside>
-      <div className="sticky top-0 bg-white z-10 pb-4">
-        <CategoryPills />
+      <div className="px-8">
+        <div className="sticky top-0 bg-white z-10 pb-4">
+          <CategoryPills categories={categories} />
+        </div>
+        <main></main>
       </div>
     </div>
   );
