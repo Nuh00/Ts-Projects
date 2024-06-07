c9311866bebb3083c0440e978a39d75c59f1d949
Nuh00 <barkatnoah35@gmail.com>
2024-06-07 18:31:26 -0500

    update

--- First-Ts/src/components/CategoryPills.tsx
+++ First-Ts/src/components/CategoryPills.tsx
@@ -6,43 +6,51 @@ type CategoryPillsProps = {
   categories: string[];
 };
 
+const TRANSLATE_AMOUNT = 200;
+
 function CategoryPills({ categories }: CategoryPillsProps) {
   const [activeCategory, setActiveCategory] = useState(categories[0]);
+  const [isLeftVisible, setIsLeftVisible] = useState(true);
+  const [isRightVisible, setIsRightVisible] = useState(true);
 
   return (
-    <div className="relative overflow-x-auto">
-      <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
-        <Button
-          size="icon"
-          variant="ghost"
-          className="h-full aspect-square w-auto p-1.5"
-        >
-          <ChevronLeft />
-        </Button>
-      </div>
+    <div className="relative">
+      {isLeftVisible && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <Button
+            size="icon"
+            variant="ghost"
+            className="h-full aspect-square w-auto p-1.5"
+          >
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
 
-      <main className="flex  w-[max-content]">
+      <main className="flex  w-[max-content] gap-2 transition-transform whitespace-nowrap overflow-x-auto">
         {categories.map((category) => (
           <Button
             onClick={() => setActiveCategory(category)}
             key={category}
             variant={category === activeCategory ? "dark" : "default"}
-            className="py-1 px-3 rounded-lg whitespace-nowrap "
+            className="py-1 px-3 rounded-lg whitespace-nowrap  "
           >
             {category}
           </Button>
         ))}
       </main>
 
-      <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-70% to-transparent w-24 h-full ">
-        <Button
-          size="icon"
-          variant="ghost"
-          className="h-full aspect-square w-auto p-1.5"
-        >
-          <ChevronRight />
-        </Button>
-      </div>
+      {isRightVisible && (
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-70% to-transparent w-24 h-full ">
+          <Button
+            size="icon"
+            variant="ghost"
+            className="h-full aspect-square w-auto p-1.5"
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
