71c5efde6d25e422a5dfd76f3961eb6f28432c7f
nuh <noahbarkatwork@gmail.com>
2024-06-24 13:39:48 -0500

    finished the skeletion for the dynamic rendering pages

--- first-next/src/app/blog/[slug]/page.tsx
+++ first-next/src/app/blog/[slug]/page.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 function SinglePostPage() {
   return (
-    <div>
-      This is the single post page
+    <div className=" flex gap-10 px-4">
+      <div className=" relative h-[700px] w-[350px]">
+        <Image
+          src="https://images.pexels.com/photos/25929113/pexels-photo-25929113/free-photo-of-a-green-frog-sits-on-a-leaf-in-the-rainforest.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          layout="fill"
+          objectFit="cover"
+        ></Image>
+      </div>
+      <div className="flex flex-col gap-6">
+        <h1 className="text-6xl font-semibold ">Title</h1>
+        <div className="flex gap-3">
+          <div className="relative rounded-full w-12 h-12 overflow-hidden">
+            <Image src="https://images.pexels.com/photos/23848646/pexels-photo-23848646/free-photo-of-a-stone-bridge-over-a-small-stream-in-the-mountains.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=loading" alt="avatar"
+              layout="fill"
+              objectFit="cover"
+             />
+          </div>
+          <div>
+            <p className=" text-gray-400">Author</p>
+            <p>Name of Author</p>
+          </div>
+          <div>
+            <p className="text-gray-400">Published</p>
+            <p>01.01.2024</p>
+          </div>
+        </div>
+        <div className="px-3">Desc</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SinglePostPage
+export default SinglePostPage;
--- first-next/src/app/blog/page.tsx
+++ first-next/src/app/blog/page.tsx
@@ -8,35 +8,35 @@ function BlogPage() {
         date="01.01.2024"
         title="Title"
         description="amazing to learn"
-        link="/"
+        link="/blog/post"
       />
       <PostCard
         image="https://images.pexels.com/photos/26125407/pexels-photo-26125407/free-photo-of-a-gray-butterfly-sitting-on-a-leaf.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         date="01.01.2024"
         title="Title"
         description="amazing to learn, amazing to see, wonderful to behold"
-        link="/"
+        link="/blog/post"
       />
       <PostCard
         image="https://images.pexels.com/photos/25929113/pexels-photo-25929113/free-photo-of-a-green-frog-sits-on-a-leaf-in-the-rainforest.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         date="01.01.2024"
         title="Title"
         description="amazing to learn"
-        link="/"
+        link="/blog/post"
       />
       <PostCard
-        image="/noavatar.png"
+        image="https://images.pexels.com/photos/24243724/pexels-photo-24243724/free-photo-of-a-photo-of-a-cloud-with-a-moon-in-the-sky.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         date="01.01.2024"
         title="Title"
         description="amazing to learn"
-        link="/"
+        link="/blog/post"
       />
       <PostCard
-        image="/noavatar.png"
+        image="https://images.pexels.com/photos/14785695/pexels-photo-14785695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         date="01.01.2024"
         title="Title"
         description="amazing to learn"
-        link="/"
+        link="/blog/post"
       />
     </div>
   );
