9818c7eafb811942114f0b64041fc2cfabb33122
nuh <noahbarkatwork@gmail.com>
2024-06-24 11:07:23 -0500

    fixed issue with postcard placement

--- first-next/src/app/blog/page.tsx
+++ first-next/src/app/blog/page.tsx
@@ -2,12 +2,42 @@ import React from "react";
 import PostCard from "@/components/PostCard/PostCard";
 function BlogPage() {
   return (
-    <div className="flex flex-wrap">
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      
-     
+    <div className="flex flex-wrap justify-center gap-4">
+      <PostCard
+        image="https://images.pexels.com/photos/23848646/pexels-photo-23848646/free-photo-of-a-stone-bridge-over-a-small-stream-in-the-mountains.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=loading"
+        date="01.01.2024"
+        title="Title"
+        description="amazing to learn"
+        link="/"
+      />
+      <PostCard
+        image="https://images.pexels.com/photos/26125407/pexels-photo-26125407/free-photo-of-a-gray-butterfly-sitting-on-a-leaf.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        date="01.01.2024"
+        title="Title"
+        description="amazing to learn, amazing to see, wonderful to behold"
+        link="/"
+      />
+      <PostCard
+        image="https://images.pexels.com/photos/25929113/pexels-photo-25929113/free-photo-of-a-green-frog-sits-on-a-leaf-in-the-rainforest.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        date="01.01.2024"
+        title="Title"
+        description="amazing to learn"
+        link="/"
+      />
+      <PostCard
+        image="/noavatar.png"
+        date="01.01.2024"
+        title="Title"
+        description="amazing to learn"
+        link="/"
+      />
+      <PostCard
+        image="/noavatar.png"
+        date="01.01.2024"
+        title="Title"
+        description="amazing to learn"
+        link="/"
+      />
     </div>
   );
 }
--- first-next/src/components/PostCard/PostCard.tsx
+++ first-next/src/components/PostCard/PostCard.tsx
@@ -2,23 +2,45 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-function PostCard() {
+{/* We want ot pass in 
+1. date
+2. title
+3. description
+4. image
+5. link
+
+ */}
+
+ type PostCardProps = {
+    date?: string;
+    title?: string;
+    description?: string;
+    image: string;
+    link: string;
+  
+ }
+
+function PostCard({ date, title, description, image, link}: PostCardProps) {
   return (
-    <div className="flex flex-col gap-4 mb-20 ">
-      <div className="flex justify-between">
-        <div className="relative w-9/10 h-[400px]">
-          <Image src="/noavatar.png" alt="" objectFit="cover"
-          width={400}
-          height={400}
-          
+    <div className="flex flex-col gap-4 mb-20 w-[400px] ">
+      <div className="flex">
+        <div className="relative w-[90%] h-[400px]">
+          <Image
+            src={image}
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            
           ></Image>
         </div>
-        <span className=" text-sm rotate-[270deg]">01.01.2024</span>
+        <div className=" flex items-center text-sm">
+          <span className=" rotate-[270deg]">{date} </span>
+        </div>
       </div>
       <div>
-        <h1>Title</h1>
-        <p>Desc</p>
-        <Link href="/">Read more</Link>
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className=" text-header-gray">{description}</p>
+        <Link href={link} className="text-white text-l  underline">Read more</Link>
       </div>
     </div>
   );
